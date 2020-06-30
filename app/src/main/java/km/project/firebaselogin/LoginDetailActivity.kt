fc05341bbfb98e2cd8525531c3234136c760a2a0
김민제 <kmj2091@naver.com>
2020-06-30 16:16:49 +0900

    Firebase Login

---
+++ app/src/main/java/km/project/firebaselogin/LoginDetailActivity.kt
@@ -0,0 +1,42 @@
+package km.project.firebaselogin
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login_detail.*
+
+class LoginDetailActivity : Activity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_detail)
+        auth = FirebaseAuth.getInstance()
+
+        btn_logout.setOnClickListener { signOut() }
+        btn_exit.setOnClickListener { signExit() }
+    }
+
+    @SuppressLint("ShowToast")
+    private fun signOut() {
+        auth.signOut()
+        MainActivity.startMainActivity(this)
+        Toast.makeText(this, "로그아웃", Toast.LENGTH_SHORT)
+    }
+
+    @SuppressLint("ShowToast")
+    private fun signExit() {
+        auth.currentUser?.delete()
+        Toast.makeText(this, "회원탈퇴", Toast.LENGTH_SHORT)
+    }
+
+    companion object {
+        fun startLoginDetailActivity(activity: Activity) {
+            activity.startActivity(Intent(activity, LoginDetailActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/km/project/firebaselogin/MainActivity.kt
@@ -0,0 +1,106 @@
+package km.project.firebaselogin
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //GoogleSignInOptions 옵션을 관리해주는 클래스로 API 키값과 요청할 값이 저장되어 있다.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_google.setOnClickListener {
+            signIn()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN && resultCode == Activity.RESULT_OK) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data) ?: return
+            print(result)
+            if (result.isSuccess) {
+                val account = result.signInAccount
+                googleLogin(account)
+            }
+        }
+    }
+
+    private fun googleLogin(acct: GoogleSignInAccount?) {
+
+        val credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    moveMain(this, user)
+                } else {
+                    showAlertDialog()
+                }
+            }.addOnFailureListener {exception ->
+                Toast.makeText(this,exception.toString(),Toast.LENGTH_SHORT)
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, SIGN_IN)
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("잠시 후 시도해 주세요.")
+            setPositiveButton("확인", null)
+            show()
+        }
+        moveMain(this, null)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        moveMain(this, currentUser)
+    }
+
+    private fun moveMain(activity: Activity, user: FirebaseUser?) {
+        if (user != null) {
+            LoginDetailActivity.startLoginDetailActivity(activity)
+        }
+    }
+
+    companion object {
+        private const val SIGN_IN = 1000
+        fun startMainActivity(activity: Activity) {
+            activity.startActivity(Intent(activity, MainActivity::class.java))
+        }
+    }
+}
